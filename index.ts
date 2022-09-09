2ceebdd95f7fec365214ca90e11f813df56dd271
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-09-09 11:54:39 -0500

    add assignment and category types

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
+export interface CourseGrade {
+  value: string;
+  key: string;
+  active: boolean;
+};
+
 export interface Course {
   key: string;
   name: string;
-  grades: ({
-    value: string;
-    key: string;
-    active: boolean;
-  } | null)[];
+  grades: (CourseGrade | null)[];
 }
 
 export interface CourseResponse {
@@ -13,4 +15,35 @@ export interface CourseResponse {
   sessionId: string;
   referer: string;
   columnNames: string[];
+}
+
+export interface Category extends CategoryDetails {
+  assignments: CategoryDetails[];
+}
+
+export interface CategoryDetails {
+  name: string;
+  id: string;
+  average: string;
+  weight: number;
+  error: boolean;
+}
+
+export interface Assignment {
+  name: string;
+  grade: string;
+  dropped: boolean;
+  assign: string;
+  due: string;
+  scale: number;
+  max: number;
+  count: number;
+  note: string;
+  error: boolean;
+}
+
+export interface CourseAssignmentsResponse {
+  sessionId: string;
+  referer: string;
+  categories: Category[];
 }
